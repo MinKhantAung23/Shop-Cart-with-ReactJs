c2d49f03d8aebb2eca85ac810ecabd779c6ea669
Min Khant Aung <minkhantaung743@gmail.com>
2024-06-03 19:36:01 +0630

    about update

--- src/page/About.jsx
+++ src/page/About.jsx
@@ -4,16 +4,15 @@ const About = () => {
   return (
     <div className="w-full h-full ">
       <h1 className="my-5 text-center text-xl font-bold text-primary">
-        About Min Khant Aung's Cart Management Shop
+        Basic Cart Management Shop
       </h1>
       <p className="tracking-wide leading-8 text-gray-500">
-        Welcome to Min Khant Aung's Cart Management Shop! I specialize in
-        creating shopping experiences using technologies like React, Redux
-        Toolkit, and RTK Query.My goal is to provide a simple solution for
-        managing carts in online store. I have chosen to use the FakeStoreAPI
-        for simulating an online store environment.There is still a lot of error
-        like error handling. At Min Khant Aung's Cart Management Shop, you can
-        see basic cart management about online shop.
+        Welcome to Basic Cart Management Shop! I specialize in creating shopping
+        experiences using technologies like React, Redux Toolkit, and RTK
+        Query.My goal is to provide a simple solution for managing carts in
+        online store. I have chosen to use the FakeStoreAPI for simulating an
+        online store environment. At this Basic Cart Management Shop, you can
+        see nothing special just basic cart management about online shop.
       </p>
       <p className="font-semibold my-3">Thank...</p>
     </div>
